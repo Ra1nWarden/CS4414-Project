7f146ee664abc4865a96a684311b3458fc6da6d5
Ra1nWarden <wzh19921016@gmail.com>
2014-04-26 22:32:23 -0400

    server passing done. Next: problem interpreting

--- client/client.py
+++ client/client.py
@@ -1,5 +1,8 @@
 import requests
 import datetime
+from subprocess import call
+import os
+import time
 
 address = "http://127.0.0.1:4414"
 
@@ -49,7 +52,7 @@ class CommandReader:
             print r.text
         else:
             print "No pending turns for you."
-    
+
     def send_msg(self):
         r = requests.get(address + '/new_round')
         print "Your word is: "
@@ -58,25 +61,79 @@ class CommandReader:
         print "Enter 2 to write your own"
         print "Enter 0 to get a new word"
         choice = raw_input("Your choice: ").strip()
+        while choice not in ["1", "2", "0"]:
+            print "Invalid command!"
+            choice = raw_input("Your choice: ").strip()
         if choice == "1":
             args = {}
             args['key'] = r.text
             art_request = requests.get(address + '/get_ascii_art', params=args)
             if art_request.status_code == 200:
                 print art_request.text
+                print "Enter 1 to submit"
+                print "Enter 2 to start writing your own instead"
+                second_choice = raw_input("Your choice: ").strip()
+                while second_choice not in ["1", "2"]:
+                    second_choice = raw_input("Your choice: ").strip()
+                if second_choice == "1":
+                    self.upload(r.text, True)
+                else:
+                    while not self.start_editor():
+                        print "Nothing entered! Please try again!"
+                        time.sleep(3)
+                    self.upload(r.text, False)
             else:
                 print "Sorry, no available ASCII art :("
+                print "Enter 0 to get a new word"
+                print "Enter 2 to start writing your own"
+                second_choice = raw_input("Your choice: ").strip()
+                while second_choice not in ["0", "2"]:
+                    second_choice = raw_input("Your choice: ").strip()
+                if second_choice == "0":
+                    self.send_msg()
+                elif second_choice == "2":
+                    while not self.start_editor():
+                        print "Nothing entered! Please try again!"
+                        time.sleep(3)
+                    self.upload(r.text, False)
         elif choice == "2":
-            print "2"
+            while not self.start_editor():
+                print "Nothing entered! Please try again!"
+                time.sleep(3)
+            self.upload(r.text, False)
         elif choice == "0":
             self.send_msg()
-        else:
-            print "Invalid command!"
-        # recipient = raw_input("Please enter the name of the recipient: ")
-        
 
+    def start_editor(self):
+        file_name = self.username + ".txt"
+        call(['vim', file_name])
+        return os.path.isfile(file_name)
+
+    def upload(self, word, ascii_option):
+        recipient = ""
+        while True:
+            recipient = raw_input("Please enter the name of the recipient: ")
+            args = {}
+            args['username'] = recipient
+            r = requests.get(address+'/check_user', params=args)
+            if r.status_code == 200:
+                break
+            else:
+                print "User does not exist! Please try again!"
+        args = {}
+        args['recipient'] = recipient
+        args['sender'] = self.username
+        args['ascii_option'] = ascii_option
+        args['word'] = word
+        if not ascii_option:
+            args['content'] = open(self.username + ".txt", 'r').read()
+        upload_request = requests.post(address + '/upload_question', params=args)
+        if upload_request.status_code == 200:
+            print "Question Sent!"
+        else:
+            print "An error occurred. Please try again."
     # def view_friends(self):
-        
+
 
 if __name__ == "__main__":
     cr = CommandReader()
--- server/zhtta.rs
+++ server/zhtta.rs
@@ -1,12 +1,10 @@
 //
 // zhtta.rs
 //
-// Starting code for PS3
+// Server for the drawsomething like multiplayer game
 // Running on Rust 0.9
 //
-// Note that this code has serious security risks!  You should not run it 
-// on any system with access to sensitive files.
-// 
+//
 // University of Virginia - cs4414 Spring 2014
 // Weilin Xu and David Evans
 // Version 0.5
@@ -39,7 +37,7 @@ static HTTP_OK : &'static str = "HTTP/1.1 200 OK\r\nContent-Type: text/html; cha
 static HTTP_BAD : &'static str = "HTTP/1.1 404 Not Found\r\n\r\n";
 
 struct HTTP_Request {
-    // Use peer_name as the key to access TcpStream in hashmap. 
+    // Use peer_name as the key to access TcpStream in hashmap.
 
     // (Due to a bug in extra::arc in Rust 0.9, it is very inconvenient to use TcpStream without the "Freeze" bound.
     //  See issue: https://github.com/mozilla/rust/issues/12139)
@@ -50,10 +48,10 @@ struct HTTP_Request {
 struct WebServer {
     ip: ~str,
     port: uint,
-    
+
     request_queue_arc: MutexArc<~[HTTP_Request]>,
     stream_map_arc: MutexArc<HashMap<~str, Option<std::io::net::tcp::TcpStream>>>,
-    
+
     notify_port: Port<()>,
     shared_notify_chan: SharedChan<()>,
 
@@ -97,12 +95,12 @@ impl WebServer {
         WebServer {
             ip: ip.to_owned(),
             port: port,
-                        
+
             request_queue_arc: MutexArc::new(~[]),
             stream_map_arc: MutexArc::new(HashMap::new()),
-            
+
             notify_port: notify_port,
-            shared_notify_chan: shared_notify_chan,  
+            shared_notify_chan: shared_notify_chan,
 
             user_map: MutexArc::new(user_map),
             question_map: MutexArc::new(problem_map),
@@ -110,64 +108,64 @@ impl WebServer {
             questions: MutexArc::new(problems),
         }
     }
-    
+
     fn run(&mut self) {
         self.listen();
         self.dequeue_static_file_request();
     }
-    
+
     fn listen(&mut self) {
         let addr = from_str::<SocketAddr>(format!("{:s}:{:u}", self.ip, self.port)).expect("Address error.");
-        
+
         let request_queue_arc = self.request_queue_arc.clone();
         let shared_notify_chan = self.shared_notify_chan.clone();
         let stream_map_arc = self.stream_map_arc.clone();
         let user_map_arc = self.user_map.clone();
         let question_map_arc = self.question_map.clone();
         let questions_arc = self.questions.clone();
-                
+
         spawn(proc() {
             let mut acceptor = net::tcp::TcpListener::bind(addr).listen();
-            println!("{:s} listening on {:s}.", 
+            println!("{:s} listening on {:s}.",
                      SERVER_NAME, addr.to_str());
-            
+
             for stream in acceptor.incoming() {
                 let (queue_port, queue_chan) = Chan::new();
                 queue_chan.send(request_queue_arc.clone());
-                
+
                 let notify_chan = shared_notify_chan.clone();
                 let stream_map_arc = stream_map_arc.clone();
                 let user_map = user_map_arc.clone();
                 let question_map = question_map_arc.clone();
                 let questions = questions_arc.clone();
-                
+
                 // Spawn a task to handle the connection.
                 spawn(proc() {
                     let request_queue_arc = queue_port.recv();
-                  
+
                     let mut stream = stream;
-                    
+
                     let mut buf = [0, ..700];
                     stream.read(buf);
                     let request_str = str::from_utf8(buf);
                     debug!("Request:\n{:s}", request_str);
-                    
+
                     let req_group : ~[&str]= request_str.splitn(' ', 3).collect();
                     if req_group.len() > 2 {
                         let path_str = "." + req_group[1].to_owned();
-                        
+
                         let mut path_obj = ~os::getcwd();
                         path_obj.push(path_str.clone());
-                        
+
                         let ext_str = match path_obj.extension_str() {
                             Some(e) => e,
                             None => "",
                         };
-                        
+
                         debug!("Requested path: [{:s}]", path_obj.as_str().expect("error"));
                         debug!("Requested path: [{:s}]", path_str);
                         debug!("ext_str is: {:s}", ext_str);
-                                
+
                         if path_str.starts_with("./login") {
                             debug!("=====in login branch====");
                             let args_vec : ~[&str] = path_str.slice_from(8).split('&').collect();
@@ -195,7 +193,32 @@ impl WebServer {
                             debug!("======generating ascii art=====");
                             let word : ~str = path_str.slice_from(20).to_owned().clone();
                             WebServer::generate_ascii_art(stream, question_map, word);
-                        } else { 
+                        } else if path_str.starts_with("./check_user") {
+                            debug!("======checking presence of user===");
+                            let username : &str = path_str.slice_from(22);
+                            WebServer::check_user_presence(stream, username, user_map);
+                        } else if path_str.starts_with("./upload_question") {
+                            debug!("=======uploading question========");
+                            let args_vec : ~[&str] = path_str.slice_from(18).split('&').collect();
+                            let mut recipient : &str = &"";
+                            let mut sender : &str = &"";
+                            let mut ascii_option : &str = &"";
+                            let mut content : &str = &"";
+                            let mut key_word : &str = &"";
+                            for each in args_vec.iter() {
+                                let splitted_pair : ~[&str] = each.split('=').collect();
+                                match splitted_pair[0] {
+                                    "recipient" => recipient = splitted_pair[1],
+                                    "sender" => sender = splitted_pair[1],
+                                    "ascii_option" => ascii_option = splitted_pair[1],
+                                    "content" => content = splitted_pair[1],
+                                    "word" => key_word = splitted_pair[1],
+                                    _ => (),
+                                };
+                            }
+                            debug!("===finished parsing arguments===");
+                            WebServer::upload_question(stream, recipient, sender, key_word, ascii_option, content);
+                        } else {
                             debug!("===== Static Page request =====");
                             WebServer::enqueue_static_file_request(stream, path_obj, stream_map_arc, request_queue_arc, notify_chan);
                         }
@@ -226,7 +249,7 @@ impl WebServer {
                 };
         });
     }
-    
+
     fn retrieve_question(stream: Option<std::io::net::tcp::TcpStream>, username: &str) {
         let mut stream = stream;
         let file_path = &Path::new(PENDING_PATH + "/" + username.to_owned() + ".txt");
@@ -235,7 +258,7 @@ impl WebServer {
                       stream.write(HTTP_OK.as_bytes());
                       stream.write(file_content.read_to_end());
             },
-            false => { debug!("does not exist"); 
+            false => { debug!("does not exist");
                        File::create(file_path);
                        stream.write(HTTP_BAD.as_bytes());
             },
@@ -275,7 +298,54 @@ impl WebServer {
             false => stream.write(HTTP_BAD.as_bytes()),
         };
     }
-    
+
+    fn check_user_presence(stream: Option<std::io::net::tcp::TcpStream>, username: &str, user_map: MutexArc<HashMap<~str, ~str>>) {
+        let (stream_port, stream_chan) = Chan::new();
+        let (username_port, username_chan) = Chan::new();
+        username_chan.send(username.to_owned());
+        stream_chan.send(stream);
+        user_map.access(|local_user_map| {
+            let received_username = username_port.recv();
+            let mut received_stream = stream_port.recv();
+            match local_user_map.find(&received_username.to_owned()) {
+              Some(_) => received_stream.write(HTTP_OK.as_bytes()),
+              None => received_stream.write(HTTP_BAD.as_bytes()),
+            };
+        });
+    }
+
+    fn upload_question(stream: Option<std::io::net::tcp::TcpStream>, recipient: &str, sender: &str, key_word: &str, ascii_option: &str, content: &str) {
+        let mut stream = stream;
+        let mut str_content : ~str = ~"sender: ";
+        str_content = str_content.append(sender);
+        str_content = str_content.append(&"\n");
+        str_content = str_content.append(&"key: ");
+        str_content = str_content.append(key_word);
+        str_content = str_content.append(&"\n");
+        debug!("content is {}", content);
+        match ascii_option {
+            "True" => str_content = str_content.append(&"ASCII\n"),
+            "False" => { str_content = str_content.append(content);
+                         str_content = str_content.append(&"\n");
+                       },
+            _ => (),
+        };
+        let file_path = &Path::new(PENDING_PATH + "/" + recipient.to_owned() + ".txt");
+        debug!("{}", PENDING_PATH + "/" + recipient.to_owned() + ".txt");
+        match file_path.exists() {
+            true => { let mut file_content = File::open_mode(file_path, Append, ReadWrite);
+                      file_content.write_str(str_content);
+                      stream.write(HTTP_OK.as_bytes());
+            },
+            false => { debug!("false");
+                       File::create(file_path);
+                       let mut file_content = File::open_mode(file_path, Append, ReadWrite);
+                       file_content.write_str(str_content);
+                       stream.write(HTTP_OK.as_bytes());
+            },
+        };
+    }
+
     // TODO: Streaming file.
     // TODO: Application-layer file caching.
     fn respond_with_static_file(stream: Option<std::io::net::tcp::TcpStream>, path: &Path) {
@@ -284,7 +354,7 @@ impl WebServer {
         stream.write(HTTP_OK.as_bytes());
         stream.write(file_reader.read_to_end());
     }
-    
+
     // TODO: Smarter Scheduling.
     fn enqueue_static_file_request(stream: Option<std::io::net::tcp::TcpStream>, path_obj: &Path, stream_map_arc: MutexArc<HashMap<~str, Option<std::io::net::tcp::TcpStream>>>, req_queue_arc: MutexArc<~[HTTP_Request]>, notify_chan: SharedChan<()>) {
         // Save stream in hashmap for later response.
@@ -299,7 +369,7 @@ impl WebServer {
                 local_stream_map.swap(peer_name.clone(), stream);
             });
         }
-        
+
         // Enqueue the HTTP request.
         let req = HTTP_Request { peer_name: peer_name.clone(), path: ~path_obj.clone() };
         let (req_port, req_chan) = Chan::new();
@@ -312,23 +382,23 @@ impl WebServer {
             local_req_queue.push(req);
             debug!("A new request enqueued, now the length of queue is {:u}.", local_req_queue.len());
         });
-        
+
         notify_chan.send(()); // Send incoming notification to responder task.
-    
-    
+
+
     }
-    
+
     // TODO: Smarter Scheduling.
     fn dequeue_static_file_request(&mut self) {
         let req_queue_get = self.request_queue_arc.clone();
         let stream_map_get = self.stream_map_arc.clone();
-        
+
         // Port<> cannot be sent to another task. So we have to make this task as the main task that can access self.notify_port.
-        
+
         let (request_port, request_chan) = Chan::new();
         loop {
             self.notify_port.recv();    // waiting for new request enqueued.
-            
+
             req_queue_get.access( |req_queue| {
                 match req_queue.shift_opt() { // FIFO queue.
                     None => { /* do nothing */ }
@@ -338,9 +408,9 @@ impl WebServer {
                     }
                 }
             });
-            
+
             let request = request_port.recv();
-            
+
             // Get stream from hashmap.
             // Use unsafe method, because TcpStream in Rust 0.9 doesn't have "Freeze" bound.
             let (stream_port, stream_chan) = Chan::new();
@@ -350,7 +420,7 @@ impl WebServer {
                     stream_chan.send(stream);
                 });
             }
-            
+
             // TODO: Spawning more tasks to respond the dequeued requests concurrently. You may need a semophore to control the concurrency.
             let stream = stream_port.recv();
             WebServer::respond_with_static_file(stream, request.path);
@@ -358,7 +428,7 @@ impl WebServer {
             debug!("=====Terminated connection from [{:s}].=====", request.peer_name);
         }
     }
-    
+
     fn get_peer_name(stream: &mut Option<std::io::net::tcp::TcpStream>) -> ~str {
         match *stream {
             Some(ref mut s) => {
@@ -379,11 +449,11 @@ fn get_args() -> (~str, uint) {
         println!("--port   \tport number, \"{:u}\" by default.", PORT);
         println("-h --help \tUsage");
     }
-    
+
     /* Begin processing program arguments and initiate the parameters. */
     let args = os::args();
     let program = args[0].clone();
-    
+
     let opts = ~[
         getopts::optopt("ip"),
         getopts::optopt("port"),
@@ -400,19 +470,19 @@ fn get_args() -> (~str, uint) {
         print_usage(program);
         unsafe { libc::exit(1); }
     }
-    
+
     let ip_str = if matches.opt_present("ip") {
                     matches.opt_str("ip").expect("invalid ip address?").to_owned()
                  } else {
                     IP.to_owned()
                  };
-    
+
     let port:uint = if matches.opt_present("port") {
                         from_str::from_str(matches.opt_str("port").expect("invalid port number?")).expect("not uint?")
                     } else {
                         PORT
                     };
-    
+
     (ip_str, port)
 }
 
